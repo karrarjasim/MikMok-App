bc073f9757332e6456bb25052ed54b127264b395
Falah Hassan <devfalahhassan@gmail.com>
2022-07-31 09:39:16 -0700

    add data modeling

---
+++ app/src/main/java/com/example/mikmok/models/MediaFeed.kt
@@ -0,0 +1,6 @@
+package com.example.mikmok.models
+
+data class MediaFeed(
+    val backgrounds: List<String>,
+    val feed: List<Feed>
+)
---
+++ app/src/main/java/com/example/mikmok/models/VideoModel.kt
@@ -0,0 +1,13 @@
+package com.example.mikmok.models
+
+data class VideoModel(
+    val art: String,
+    val description: String,
+    val director: String,
+    val duration: Int,
+    val id: String,
+    val ratings: List<String>?,
+    val title: String,
+    val url: String,
+    val year: Int
+)
