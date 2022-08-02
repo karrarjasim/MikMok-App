b51defc007032b44bbb4942f0347417ff27b0461
A10XY <86065368+A10XY@users.noreply.github.com>
2022-08-02 18:40:18 +0300

    edit exo player with enhanced features and fix some bugs

--- app/src/main/java/com/example/mikmok/ui/VideoAdapter.kt
+++ app/src/main/java/com/example/mikmok/ui/VideoAdapter.kt
@@ -1,26 +1,22 @@
 package com.example.mikmok.ui
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.example.mikmok.R
-import com.example.mikmok.data.models.ExoPlayerItem
 import com.example.mikmok.data.models.VideoModel
 import com.example.mikmok.databinding.SingleVideoBinding
 import com.example.mikmok.util.OnVideoClickListener
-import com.example.mikmok.util.OnVideoPreparedListener
 import com.google.android.exoplayer2.ExoPlayer.Builder
 import com.google.android.exoplayer2.MediaItem
 import com.google.android.exoplayer2.PlaybackException
 import com.google.android.exoplayer2.Player
 
-
 class VideoAdapter(
     private val videoList: List<VideoModel>,
-    private val videoPreparedListener: OnVideoPreparedListener,
     private val onVideoClickListener : OnVideoClickListener
     ) : RecyclerView.Adapter<VideoAdapter.VideoViewHolder>() {
 
@@ -35,7 +31,7 @@ class VideoAdapter(
 
     override fun onBindViewHolder(holder: VideoViewHolder, position: Int) {
         holder.binding.apply {
-            setupPlayer(videoList[position].url, position)
+            setupPlayer(videoList[position].url)
             bindVideoView(videoList[position])
         }
     }
@@ -48,7 +44,7 @@ class VideoAdapter(
         Glide.with(root).load(currentVideo.art).into(image)
     }
 
-    private fun SingleVideoBinding.setupPlayer(url: String, position: Int) {
+    private fun SingleVideoBinding.setupPlayer(url: String) {
         Builder(root.context).build().run {
             videoView.player = this
             addListener(object : Player.Listener {
@@ -60,20 +56,21 @@ class VideoAdapter(
                         Player.STATE_READY -> {
                             progressBar.visibility = View.INVISIBLE
                         }
+                        Player.STATE_ENDED -> {
+                            Log.d(TAG, "Video Ended")
+                        }
+                        Player.STATE_IDLE -> {
+                            Log.d(TAG, "Video Idle")
+                        }
                     }
                 }
                 override fun onPlayerError(error: PlaybackException) {
                     super.onPlayerError(error)
-                    Toast.makeText(root.context, "Could not play video due to: $error", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(root.context, "Could not play video due to: ${error.message}", Toast.LENGTH_SHORT).show()
                 }
             })
             addMediaItem(MediaItem.fromUri(url))
             prepare()
-            if (position == 0) {
-                playWhenReady = true
-                play()
-            }
-            videoPreparedListener.onVideoPrepared(ExoPlayerItem(this, position))
             videoView.videoSurfaceView?.setOnClickListener {
                 onVideoClickListener.onClick(this)
             }
@@ -81,4 +78,18 @@ class VideoAdapter(
     }
 
     override fun getItemCount() = videoList.size
+
+    override fun onViewAttachedToWindow(holder: VideoViewHolder) {
+        super.onViewAttachedToWindow(holder)
+        holder.binding.videoView.player?.play()
+    }
+
+    override fun onViewDetachedFromWindow(holder: VideoViewHolder) {
+        super.onViewDetachedFromWindow(holder)
+        holder.binding.videoView.player?.pause()
+    }
+
+    companion object {
+        const val TAG = "VIDEO_ADAPTER_LOG_TAG"
+    }
 }
--- app/src/main/java/com/example/mikmok/util/OnVideoPreparedListener.kt
+++
@@ -1,7 +0,0 @@
-package com.example.mikmok.util
-
-import com.example.mikmok.data.models.ExoPlayerItem
-
-interface OnVideoPreparedListener {
-    fun onVideoPrepared(exoPlayerItem: ExoPlayerItem)
-}
